d2610fd53ed0760fbbc2a385934ae2e489831335
amakomaya <amakomaya2012@gmail.com>
2020-10-26 10:54:00 +0545

    Thank you for this work

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-echo "this is hello world in php this is updated verwion in second update. I am adding it in thrid time";
+echo "this is hello world in php this is updated verwion in second update. I am adding it in thrid time Thank you Git, and Jenkins and Www server you have made my work easy";
 ?>
